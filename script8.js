48a8e1c458abdbe3d3f0fa5f47393051989206d6
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2024-08-29 19:08:39 +0200

    Fertiges Fotogram mit 4 Alben

---
+++ script8.js
@@ -0,0 +1,436 @@
+let portugal = [
+    `./img/portugal2021/`,
+    [
+        "IMG-20240815-WA0025.jpg",
+        "IMG-20240815-WA0026.jpg",
+        "IMG-20240815-WA0027.jpg",
+        "IMG-20240815-WA0028.jpg",
+        "IMG-20240815-WA0029.jpg",
+        "IMG-20240815-WA0030.jpg",
+        "IMG-20240815-WA0031.jpg",
+        "IMG-20240815-WA0032.jpg",
+        "IMG-20240815-WA0033.jpg",
+        "IMG-20240815-WA0034.jpg",
+        "IMG-20240815-WA0035.jpg",
+        "IMG-20240815-WA0036.jpg",
+        "IMG-20240815-WA0037.jpg",
+        "IMG-20240815-WA0038.jpg",
+        "IMG-20240815-WA0039.jpg",
+        "IMG-20240815-WA0040.jpg",
+        "IMG-20240815-WA0041.jpg",
+        "IMG-20240815-WA0042.jpg",
+        "IMG-20240815-WA0043.jpg",
+        "IMG-20240815-WA0044.jpg",
+        "IMG-20240815-WA0045.jpg",
+        "IMG-20240815-WA0046.jpg",
+        "IMG-20240815-WA0047.jpg",
+        "IMG-20240815-WA0048.jpg",
+        "IMG-20240815-WA0049.jpg",
+        "IMG-20240815-WA0050.jpg",
+        "IMG-20240815-WA0051.jpg",
+        "IMG-20240815-WA0052.jpg",
+        "IMG-20240815-WA0053.jpg",
+        "IMG-20240815-WA0054.jpg"
+    ],
+    [
+        "Praia da Falésia",
+        "Praia da Falésia",
+        "Loulé",
+        "Loulé",
+        "Loulé",
+        "Loulé",
+        "Loulé",
+        "Loulé",
+        "Praia da Cordoama",
+        "Praia da Falésia",
+        "Praia da Falésia",
+        "Lagos",
+        "Sagres",
+        "Sagres",
+        "Albufeira",
+        "Albufeira",
+        "Albufeira",
+        "Lagos",
+        "Lagos",
+        "Albufeira",
+        "Praia da Cordoama",
+        "Praia da Falésia",
+        "Praia da Falésia",
+        "Praia da Falésia",
+        "Lagos",
+        "Lagos",
+        "Albufeira",
+        "Albufeira",
+        "Praia da Falésia",
+        "Sagres",
+    ],
+    "Portugal 2021"
+]
+let mallorca = [
+    `./img/mallorca2021/`,
+    [
+        "IMG-20240818-WA0008.jpg",
+        "IMG-20240818-WA0009.jpg",
+        "IMG-20240818-WA0010.jpg",
+        "IMG-20240818-WA0011.jpg",
+        "IMG-20240818-WA0012.jpg",
+        "IMG-20240818-WA0013.jpg",
+        "IMG-20240818-WA0014.jpg",
+        "IMG-20240818-WA0015.jpg",
+        "IMG-20240818-WA0016.jpg",
+        "IMG-20240818-WA0017.jpg",
+        "IMG-20240818-WA0018.jpg",
+        "IMG-20240818-WA0019.jpg",
+        "IMG-20240818-WA0020.jpg",
+        "IMG-20240818-WA0021.jpg",
+        "IMG-20240818-WA0022.jpg",
+        "IMG-20240818-WA0023.jpg",
+        "IMG-20240818-WA0024.jpg",
+        "IMG-20240818-WA0025.jpg",
+        "IMG-20240818-WA0026.jpg",
+        "IMG-20240818-WA0027.jpg",
+        "IMG-20240818-WA0028.jpg",
+        "IMG-20240818-WA0029.jpg",
+        "IMG-20240818-WA0030.jpg",
+        "IMG-20240818-WA0031.jpg",
+        "IMG-20240818-WA0032.jpg",
+        "IMG-20240818-WA0033.jpg",
+        "IMG-20240818-WA0034.jpg",
+        "IMG-20240818-WA0035.jpg",
+        "IMG-20240818-WA0036.jpg"
+    ],
+    [
+        "Porto Colom",
+        "Porto Colom",
+        "Ausblick aus dem Zimmer",
+        "Ausblick aus dem Zimmer",
+        "Porto Colom",
+        "Porto Colom",
+        "Porto Colom",
+        "Porto Colom",
+        "Platja de Cala Marçal",
+        "Porto Colom",
+        "Porto Colom",
+        "Porto Colom",
+        "Porto Colom",
+        "Porto Colom",
+        "Arenal de ses Dones",
+        "Porto Colom",
+        "Porto Colom",
+        "Ausblick aus dem Zimmer",
+        "Platja de Cala Marçal",
+        "Ausblick aus dem Zimmer",
+        "Ausblick vom Hotel",
+        "Platja de Cala Marçal",
+        "Platja de Cala Marçal",
+        "Porto Colom",
+        "Einstieg ins Meer am Hotel",
+        "Platja de Cala Marçal",
+        "Porto Colom",
+        "Selfie :)",
+        "Arenal de ses Dones",
+    ],
+    "Mallorca 2021"
+]
+let tuerkei = [
+    `./img/tuerkei2022/`,
+    [
+        "IMG-20240818-WA0037.jpg",
+        "IMG-20240818-WA0038.jpg",
+        "IMG-20240818-WA0039.jpg",
+        "IMG-20240818-WA0040.jpg",
+        "IMG-20240818-WA0041.jpg",
+        "IMG-20240818-WA0042.jpg",
+        "IMG-20240818-WA0043.jpg",
+        "IMG-20240818-WA0044.jpg",
+        "IMG-20240818-WA0045.jpg",
+        "IMG-20240818-WA0046.jpg",
+        "IMG-20240818-WA0047.jpg",
+        "IMG-20240818-WA0048.jpg",
+        "IMG-20240818-WA0049.jpg",
+        "IMG-20240818-WA0050.jpg",
+        "IMG-20240818-WA0051.jpg",
+        "IMG-20240818-WA0052.jpg",
+        "IMG-20240818-WA0053.jpg",
+        "IMG-20240818-WA0054.jpg",
+        "IMG-20240818-WA0055.jpg",
+        "IMG-20240818-WA0056.jpg",
+        "IMG-20240818-WA0057.jpg",
+        "IMG-20240818-WA0058.jpg",
+        "IMG-20240818-WA0059.jpg",
+        "IMG-20240818-WA0060.jpg",
+        "IMG-20240818-WA0061.jpg",
+        "IMG-20240818-WA0062.jpg",
+        "IMG-20240818-WA0063.jpg",
+        "IMG-20240818-WA0064.jpg",
+        "IMG-20240818-WA0065.jpg",
+        "IMG-20240818-WA0066.jpg"
+    ],
+    [
+        "Kappadokien",
+        "Kappadokien",
+        "Kappadokien",
+        "Kappadokien",
+        "Kappadokien",
+        "Kappadokien",
+        "Kappadokien",
+        "Kappadokien",
+        "Bodrum",
+        "Kappadokien",
+        "Bodrum",
+        "Kappadokien",
+        "Bodrum",
+        "Bodrum",
+        "Kappadokien",
+        "Kappadokien",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Bodrum",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Istanbul",
+        "Bodrum",
+        "Istanbul",
+    ],
+    "Türkei 2022"
+]
+let indien = [
+    `./img/indien2023/`,
+    [
+        "IMG-20240818-WA0067.jpg",
+        "IMG-20240818-WA0068.jpg",
+        "IMG-20240818-WA0069.jpg",
+        "IMG-20240818-WA0070.jpg",
+        "IMG-20240818-WA0071.jpg",
+        "IMG-20240818-WA0072.jpg",
+        "IMG-20240818-WA0073.jpg",
+        "IMG-20240818-WA0074.jpg",
+        "IMG-20240818-WA0075.jpg",
+        "IMG-20240818-WA0076.jpg",
+        "IMG-20240818-WA0077.jpg",
+        "IMG-20240818-WA0078.jpg",
+        "IMG-20240818-WA0079.jpg",
+        "IMG-20240818-WA0080.jpg",
+        "IMG-20240818-WA0081.jpg",
+        "IMG-20240818-WA0082.jpg",
+        "IMG-20240818-WA0083.jpg",
+        "IMG-20240818-WA0084.jpg",
+        "IMG-20240818-WA0085.jpg",
+        "IMG-20240818-WA0086.jpg",
+        "IMG-20240818-WA0087.jpg",
+        "IMG-20240818-WA0088.jpg",
+        "IMG-20240818-WA0089.jpg",
+        "IMG-20240818-WA0090.jpg",
+        "IMG-20240818-WA0091.jpg",
+        "IMG-20240818-WA0092.jpg",
+        "IMG-20240818-WA0093.jpg",
+        "IMG-20240818-WA0094.jpg",
+        "IMG-20240818-WA0095.jpg",
+        "IMG-20240818-WA0096.jpg",
+        "IMG-20240818-WA0097.jpg"
+    ],
+    [
+        "Kochi",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Alappuzha",
+        "Mararikulam",
+        "Mararikulam",
+        "Mararikulam",
+        "Kochi",
+        "Mararikulam",
+        "Mararikulam",
+        "Agra",
+        "Mararikulam",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra",
+        "Agra"
+    ],
+    "Indien 2023"
+]
+
+let imgOverview = document.getElementsByClassName("imgOverview");
+let currentCountSpan = document.getElementById("currentCount");
+let totalCountSpan = document.getElementById("totalCount");
+let imgOverlay = document.getElementById("overlayCtn");
+let btL = document.querySelector(".buttonLeft")
+let btR = document.querySelector(".buttonRight")
+let chooseCollection = document.getElementById("headerNav");
+
+function startOverview(array) {
+    imgOverview[0].innerHTML = "";
+    for (let i = 0; i < array[1].length; i++) {
+        imgOverview[0].innerHTML += `<img onclick="openOverlay(event)" id="img${i}" class="img imgSmall" src="${array[0]}${array[1][i]}" alt="${array[2][i]}">`;
+    } 
+    document.getElementById("portugal2021Label").classList.add("styleTitle");
+    document.getElementById("portugal2021").checked = true;
+    document.getElementById("titleCollection").innerHTML = array[3];
+}
+
+function setOverview(array) {
+    imgOverview[0].innerHTML = "";
+    for (let i = 0; i < array[1].length; i++) {
+        imgOverview[0].innerHTML += `<img onclick="openOverlay(event)" id="img${i}" class="img imgSmall" src="${array[0]}${array[1][i]}" alt="${array[2][i]}">`;
+    }   
+    document.getElementById("titleCollection").innerHTML = array[3];
+    document.getElementById("collectionsNav").style.top = "-50px";
+    document.getElementById("collectionsNav").style.opacity = 0;
+    document.getElementById("arrowUpDown").src = "./img/icons8-expand-arrow-32.png";
+    document.getElementById("headerNav").classList.add("closed");
+}
+
+function checkMenu() {
+    let collectionsNav = document.getElementById("collectionsNav");
+    let collectionsNavText = document.getElementById("headerNav");
+    let collectionsNavUpDown = document.getElementById("arrowUpDown");
+
+    if(collectionsNavText.classList.contains("closed")) {
+        showMenu(collectionsNav, collectionsNavText, collectionsNavUpDown);
+    } else if(!collectionsNavText.classList.contains("closed")) {
+        showMenuNot(collectionsNav, collectionsNavText, collectionsNavUpDown)
+    }
+}
+
+function showMenu(collectionsNav, collectionsNavText, collectionNavUpDown) {
+    collectionsNav.style.top = "90px";
+    collectionsNav.style.opacity = 1;
+    collectionNavUpDown.src = "./img/icons8-collapse-arrow-32.png"
+    collectionsNavText.classList.remove("closed")
+}
+
+function showMenuNot(collectionsNav, collectionsNavText, collectionNavUpDown) {
+    collectionsNav.style.top = "-50px";
+    collectionsNav.style.opacity = 0;
+    collectionNavUpDown.src = "./img/icons8-expand-arrow-32.png"
+    collectionsNavText.classList.add("closed")
+}
+
+function checkbox(e) {
+    let checkboxes = document.querySelectorAll(".checkbox");
+    let clickedId = e.target.id;
+    let labels = document.querySelectorAll(".label");
+    for (let i = 0; i < checkboxes.length; i++) {
+        labels[i].classList.remove("styleTitle");
+        checkboxes[i].checked = false;
+    }
+    document.getElementById(`${clickedId}Label`).classList.add("styleTitle");
+    document.getElementById(`${clickedId}`).checked = true;
+}
+
+function openOverlay(e) {
+    document.getElementById("overlayCtn").classList.remove("dNone");
+    e.stopPropagation();
+    let clickEvent = e;
+    let clickedId = clickEvent.target.id;
+    let clickedIdNo = parseInt(clickedId.slice(3));
+    let clickedOuterHTML = e.target.outerHTML;
+    let currentArray = checkArray(clickedOuterHTML)
+    setImage(clickedIdNo, currentArray)
+}
+
+function setImage(clickedIdNo, currentArray) {
+    let srcString = `${currentArray[0]}${currentArray[1][clickedIdNo]}`
+    document.getElementById("imgBig").src = srcString;
+    let titleString = `${currentArray[2][clickedIdNo]}`
+    document.getElementById("imgTitle").innerHTML = titleString;
+    let currentCount = clickedIdNo + 1;
+    document.getElementById("currentCount").innerHTML = currentCount;
+    let totalCount = currentArray[1].length;
+    document.getElementById("totalCount").innerHTML = totalCount;
+}
+
+function checkArray(clickedOuterHTML) {
+    if (clickedOuterHTML.includes("portugal")) {
+        return portugal
+    } else if (clickedOuterHTML.includes("mallorca")) {
+        return mallorca
+    } else if (clickedOuterHTML.includes("tuerkei")) {
+        return tuerkei
+    } else if (clickedOuterHTML.includes("indien")) {
+        return indien
+    }
+}
+
+function closeOverlay(e) {
+    e.stopPropagation();
+    if (e.target.id === "overlayCtn" || e.target.id === "close") {
+        imgOverlay.classList.add("dNone");
+    }
+}
+
+function checkNextImg(numberUpDown) {
+    let oneUpDown = numberUpDown;
+    let openedImgOuterHTML = document.getElementById("imgBig").outerHTML;
+    let openedArray = checkOpenedArray(openedImgOuterHTML)
+    let openedImg = checkOpendedImg(openedImgOuterHTML, openedArray);
+    let nextImgIndex = checkNextIndex(openedArray, openedImg, oneUpDown)
+    let nextSrcString = `${openedArray[0]}${openedArray[1][nextImgIndex]}`
+    let titleString = `${openedArray[2][nextImgIndex]}`
+    let nextCount = nextImgIndex + 1;
+    let totalCount = openedArray[1].length;
+    openNextImg(nextSrcString, titleString, nextCount, totalCount)
+}
+
+function checkNextIndex(openedArray, openedImg, oneUpDown) {
+    let openedImgIndex = checkOpenedIndex(openedArray, openedImg)
+    let newIndex = openedImgIndex + oneUpDown
+    if (newIndex === -1) {
+        newIndex = openedArray[1].length -1;
+    } else if (newIndex === openedArray[1].length) {
+        newIndex = 0
+    }
+    return newIndex
+}
+
+function openNextImg(nextSrcString, titleString, nextCount, totalCount) {
+    document.getElementById("imgBig").src = nextSrcString;
+    document.getElementById("imgTitle").innerHTML = titleString;
+    document.getElementById("currentCount").innerHTML = nextCount;
+    document.getElementById("totalCount").innerHTML = totalCount;
+}
+
+function checkOpenedArray(openedImgOuterHTML) {
+    if (openedImgOuterHTML.includes("portugal")) {
+        return portugal
+    } else if (openedImgOuterHTML.includes("mallorca")) {
+        return mallorca
+    } else if (openedImgOuterHTML.includes("tuerkei")) {
+        return tuerkei
+    } else if (openedImgOuterHTML.includes("indien")) {
+        return indien
+    }
+}
+
+function checkOpendedImg(openedImgOuterHTML, openedArray) {
+    for (let i = 0; i < openedArray[1].length; i++) {
+        if (openedImgOuterHTML.includes(openedArray[1][i])) {
+            return openedArray[1][i]
+        } 
+    }
+}
+
+function checkOpenedIndex(openedArray, openedImg) {
+    return openedArray[1].indexOf(openedImg);
+}
